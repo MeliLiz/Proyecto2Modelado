297ae50b56a1d97618f9137e3fffcdfb80275986
MeliLiz <melifernandez@ciencias.unam.mx>
2022-11-05 21:51:50 -0600

    Cambie el metodo de deposito de la cuenta basica para que cobre un porcentaje al depositar cienta cantidad

--- CuentaBasica.java
+++ CuentaBasica.java
@@ -1,33 +1,43 @@
+/**
+ * Clase de una cuenta básica
+ * Esta cuenta tiene un límite de retiro y cobra un interés al depositar más de una cantidad determinada
+ */
 public class CuentaBasica extends CuentaBancaria {
     private double limiteDeRetiro;
-    private double limiteDeSaldo;
+    private double interesDeposito;
+    private double pivoteInteres;//la cantidad de depósito a partir de la cual se cobra el interés
 
+    
     /**
      * Constructor
      * 
      * @param beneficiario El nombre del beneficiario
      * @param numCuenta    El numero de cuentaBancaria
-     * @param cvv          el cvv
+     * @param cvv          El cvv de la cuenta bancaria, es con lo que se verifica la cuenta
      * @param saldo        El saldo de la cuenta
      */
     public CuentaBasica(String beneficiario, Long numCuenta, int cvv, double saldo) {
         super(beneficiario, numCuenta, cvv, saldo);
-        this.limiteDeRetiro = 3000;
-        this.limiteDeSaldo = 10000;
+        limiteDeRetiro = 3000;
+        interesDeposito = 0.03;
+        pivoteInteres=2000;
     }
 
     /**
-     * Método para depositar dinero en la cuenta
+     * Método para depositar dinero en la cuenta. Esta cuenta cobra el 3% del depósito si la cantidad a depositar es mayor a $2000
      * 
      * @param cantidad La cantidad a depositar
      * @return True si la operación fue exitosa, false en otro caso
      */
     @Override
     public boolean depositar(double cantidad) {
-        if (cantidad < 0 || (cantidad + getSaldo() > getLimiteDeSaldo())) {
+        if (cantidad < 0){
             return false;
+        }else if(cantidad>pivoteInteres){
+            setSaldo(cantidad-(interesDeposito*cantidad));
+        }else{
+            setSaldo(getSaldo() + cantidad);
         }
-        setSaldo(getSaldo() + cantidad);
         return true;
     }
 
@@ -65,21 +75,35 @@ public class CuentaBasica extends CuentaBancaria {
     }
 
     /**
-     * Método para obtener el límite de saldo de la cuenta
-     * 
-     * @return El límite de saldo de la cuenta
+     * Método para obtener el interés que se cobra al depositar más de la cantidad de pivoteInteres
+     * @return double El interés del depósito
      */
-    public double getLimiteDeSaldo() {
-        return this.limiteDeSaldo;
+    public double getInteresDeposito() {
+        return this.interesDeposito;
     }
 
     /**
-     * Método para cambiar el límite de saldo de la cuenta
-     * 
-     * @param limiteDeSaldo El nuevo límite de saldo de la cuenta
+     * Método para cambiar la cantidad de interés que se cobrará al depositar más de la cantidad del pivoteInteres
+     * @param interesDeposito La cantidad de interés
      */
-    public void setLimiteDeSaldo(double limiteDeSaldo) {
-        this.limiteDeSaldo = limiteDeSaldo;
+    public void setInteresDeposito(double interesDeposito) {
+        this.interesDeposito = interesDeposito;
     }
+
+    /**
+     * Método para obtener la cantidad de depósito a partir de la cual se cobrará el interés
+     * @return double la cantidad a partir de la cual se cobra el inteŕes del depósito
+     */
+    public double getPivoteInteres() {
+        return pivoteInteres;
+    }
+
+    /**
+     * Método para cambiar la cantidad de depósito a partir de la cual se cobrará el interés
+     * @param pivoteInteres La cantidad a partir de la cual se cobra el inteŕes del depósito
+     */
+    public void setPivoteInteres(double pivoteInteres) {
+        this.pivoteInteres = pivoteInteres;
+    }   
 
 }
--- CuentaPremium.java
+++ CuentaPremium.java
@@ -1,3 +1,7 @@
+/**
+ * Clase que simula una cuenta bancaria premium
+ * Este tipo de cuenta no tiene restricciones en depósitos ni retiros
+ */
 public class CuentaPremium extends CuentaBancaria {
 
     /**
@@ -16,7 +20,7 @@ public class CuentaPremium extends CuentaBancaria {
     /**
      * Método para depositar dinero en la cuenta
      * 
-     * @param cantidad La cantidad a depositar
+     * @param cantidad La cantidad a depositar, debe ser una cantidad positiva
      * @return True si la operación fue exitosa, false en otro caso
      */
     @Override
@@ -28,6 +32,11 @@ public class CuentaPremium extends CuentaBancaria {
         return true;
     }
 
+    /**
+     * Método para retirar dinero de la cuenta.
+     * @param cantidad La cantidad a depositar, debe ser una cantidad positiva
+     * @return boolean true si la operación fue exitosa, false en otro caso
+     */
     @Override
     public boolean retirar(double cantidad) {
         if (cantidad < 0 || cantidad > getSaldo()) {
